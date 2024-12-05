7086061edfc0231572e00b8b3f66e0f751d009a3
Benjamin <benjaminbelloeil03@gmail.com>
2024-12-04 22:52:36 -0600

    Refactor task management components and implement task service for better state handling

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,19 +1,22 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
+import { DatePipe } from '@angular/common';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TasksComponent {
   @Input({required: true }) task!: Task; 
-  @Output() complete = new EventEmitter<string>();
+  private taskService = inject(TasksService);
 
   onCompleteTask(){
-    this.complete.emit(this.task.id);
+    this.taskService.removeTask(this.task.id);
   }
 
 }
